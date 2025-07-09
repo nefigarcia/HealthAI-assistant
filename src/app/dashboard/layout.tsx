874d0429d6c258a99059af9c12fc5210d8f48125
dimitrimax <finegar@gmail.com>
2025-07-09 03:00:04 +0000

    is giving this error in line 24 Console Error Server
    Error: Route /dash

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -7,6 +7,8 @@ import { getCurrentUser } from "@/services/user";
 import { Menu } from "lucide-react";
 import Link from "next/link";
 
+export const dynamic = 'force-dynamic';
+
 export default async function DashboardLayout({
   children,
 }: {
